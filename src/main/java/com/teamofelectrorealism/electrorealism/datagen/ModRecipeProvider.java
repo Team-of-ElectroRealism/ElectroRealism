086543b6a1b5933a43bcdf14b0ddf5a1d764ba81
Vetle Hartmark Jensen <vetle.h.jensen03@gmail.com>
2025-02-03 18:37:01 +0100

    Add a improved model

--- src/main/java/com/teamofelectrorealism/electrorealism/datagen/ModRecipeProvider.java
+++ src/main/java/com/teamofelectrorealism/electrorealism/datagen/ModRecipeProvider.java
@@ -29,6 +29,14 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .define('O', Items.IRON_NUGGET)
                 .unlockedBy(getHasName(ModBlocks.PROGRAMMER_BLOCK.get()), has(ModBlocks.PROGRAMMER_BLOCK.get()))
                 .save(recipeOutput);
-        shaped(RecipeCategory.)
+        shaped(RecipeCategory.MISC, ModBlocks.ARC_FURNACE)
+                .pattern("CGC")
+                .pattern("BGB")
+                .pattern("BBB")
+                .define('C', Items.COPPER_INGOT)
+                .define('G', Items.COAL)
+                .define('B', Items.BRICK)
+                .unlockedBy(getHasName(ModBlocks.ARC_FURNACE.get()), has(ModBlocks.ARC_FURNACE.get()))
+                .save(recipeOutput);
     }
 }
