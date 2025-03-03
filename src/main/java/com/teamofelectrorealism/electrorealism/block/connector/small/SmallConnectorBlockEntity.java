23d54648948c5750fb43c1e1350c2a536b4397c9
D4nders <108522590+D4nders@users.noreply.github.com>
2025-03-03 16:49:00 +0100

    Fix wire offset per rotation SmallConnectorBlockEntity

--- src/main/java/com/teamofelectrorealism/electrorealism/block/connector/small/SmallConnectorBlockEntity.java
+++ src/main/java/com/teamofelectrorealism/electrorealism/block/connector/small/SmallConnectorBlockEntity.java
@@ -12,12 +12,12 @@ import net.minecraft.world.phys.Vec3;
 public class SmallConnectorBlockEntity extends AbstractConnectorBlockEntity {
 
     private final static float OFFSET_HEIGHT = 3f;
-    public final static Vec3 OFFSET_DOWN = new Vec3(0f, -OFFSET_HEIGHT/16f, 0f);
-    public final static Vec3 OFFSET_UP = new Vec3(0f, OFFSET_HEIGHT/16f, 0f);
-    public final static Vec3 OFFSET_NORTH = new Vec3(0f, 0f, -OFFSET_HEIGHT/16f);
-    public final static Vec3 OFFSET_WEST = new Vec3(-OFFSET_HEIGHT/16f, 0f, 0f);
-    public final static Vec3 OFFSET_SOUTH = new Vec3(0f, 0f, OFFSET_HEIGHT/16f);
-    public final static Vec3 OFFSET_EAST = new Vec3(OFFSET_HEIGHT/16f, 0f, 0f);
+    public final static Vec3 OFFSET_DOWN = new Vec3(0f, OFFSET_HEIGHT/16f, 0f);
+    public final static Vec3 OFFSET_UP = new Vec3(0f, -OFFSET_HEIGHT/16f, 0f);
+    public final static Vec3 OFFSET_NORTH = new Vec3(0f, 0f, OFFSET_HEIGHT/16f);
+    public final static Vec3 OFFSET_WEST = new Vec3(OFFSET_HEIGHT/16f, 0f, 0f);
+    public final static Vec3 OFFSET_SOUTH = new Vec3(0f, 0f, -OFFSET_HEIGHT/16f);
+    public final static Vec3 OFFSET_EAST = new Vec3(-OFFSET_HEIGHT/16f, 0f, 0f);
 
     public SmallConnectorBlockEntity(BlockPos pos, BlockState blockState) {
         super(ModBlockEntityTypes.SMALL_CONNECTOR_BE.get(), pos, blockState);
