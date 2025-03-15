799ab796c80867aa98b5a267f1b632a9d6d1f109
D4nders <108522590+D4nders@users.noreply.github.com>
2025-03-15 17:30:07 +0100

    Add ModEvents

---
+++ src/main/java/com/teamofelectrorealism/electrorealism/event/ModEvents.java
@@ -0,0 +1,28 @@
+package com.teamofelectrorealism.electrorealism.event;
+
+import com.mojang.logging.LogUtils;
+import com.teamofelectrorealism.electrorealism.network.NetworkManager;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.level.LevelEvent;
+import org.slf4j.Logger;
+
+@EventBusSubscriber
+public class ModEvents {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    @SubscribeEvent
+    public static void onLevelLoad(LevelEvent.Load event) {
+        LevelAccessor level = event.getLevel();
+
+        if (level instanceof Level) {
+            if (!NetworkManager.instances.containsKey(level)) {
+                NetworkManager networkManager = new NetworkManager(level);
+                NetworkManager.instances.put(level, networkManager);
+                LOGGER.info("NetworkManager created for level: " + level);
+            }
+        }
+    }
+}
