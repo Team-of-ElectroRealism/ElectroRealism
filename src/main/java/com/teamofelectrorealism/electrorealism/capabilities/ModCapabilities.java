985c5e634df57200fdd498d5060b6cb90b284c32
D4nders <108522590+D4nders@users.noreply.github.com>
2025-04-27 14:54:08 +0200

    add working PowerImporterBlock

--- src/main/java/com/teamofelectrorealism/electrorealism/capabilities/ModCapabilities.java
+++ src/main/java/com/teamofelectrorealism/electrorealism/capabilities/ModCapabilities.java
@@ -3,6 +3,7 @@ package com.teamofelectrorealism.electrorealism.capabilities;
 import com.teamofelectrorealism.electrorealism.ElectroRealism;
 import com.teamofelectrorealism.electrorealism.block.ModBlockEntityTypes;
 import com.teamofelectrorealism.electrorealism.block.machine.consumer.power_exporter.PowerExporterBlockEntity;
+import com.teamofelectrorealism.electrorealism.block.machine.provider.power_importer.PowerImporterBlockEntity;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.capabilities.Capabilities;
@@ -17,6 +18,12 @@ public class ModCapabilities {
                 Capabilities.EnergyStorage.BLOCK,
                 ModBlockEntityTypes.POWER_EXPORTER_BE.get(),
                 PowerExporterBlockEntity::getEnergyStorage
+        );
+
+        event.registerBlockEntity(
+                Capabilities.EnergyStorage.BLOCK,
+                ModBlockEntityTypes.POWER_IMPORTER_BE.get(),
+                PowerImporterBlockEntity::getEnergyStorage
         );
     }
 }
