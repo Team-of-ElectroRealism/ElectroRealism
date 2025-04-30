a61015241d3347ecb0f4da80873e354e7d21437c
D4nders <108522590+D4nders@users.noreply.github.com>
2025-04-30 14:35:30 +0200

    add new models importer/exporter

--- src/main/java/com/teamofelectrorealism/electrorealism/block/machine/consumer/power_exporter/PowerExporterBlockEntity.java
+++ src/main/java/com/teamofelectrorealism/electrorealism/block/machine/consumer/power_exporter/PowerExporterBlockEntity.java
@@ -9,7 +9,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.energy.IEnergyStorage;
