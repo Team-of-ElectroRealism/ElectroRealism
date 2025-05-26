9a1c94b792b1d5b6c71aa8070a6f138dc749b132
D4nders <108522590+D4nders@users.noreply.github.com>
2025-05-26 03:40:51 +0200

    working JEI integration missing refinery, arcfurnace recipe types need custom gui for jei

--- src/main/java/com/teamofpowersim/powersim/justenoughitems/JEIPowerSimPlugin.java
+++ src/main/java/com/teamofpowersim/powersim/justenoughitems/JEIPowerSimPlugin.java
@@ -6,6 +6,7 @@ import com.teamofpowersim.powersim.recipe.ModRecipes;
 import com.teamofpowersim.powersim.recipe.crusher.ElectricCrusherRecipe;
 import com.teamofpowersim.powersim.screen.crusher.ElectricCrusherScreen;
 import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.registration.IGuiHandlerRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
@@ -16,6 +17,7 @@ import net.minecraft.world.item.crafting.RecipeManager;
 
 import java.util.List;
 
+@JeiPlugin
 public class JEIPowerSimPlugin implements IModPlugin {
     @Override
     public ResourceLocation getPluginUid() {
@@ -24,16 +26,17 @@ public class JEIPowerSimPlugin implements IModPlugin {
 
     @Override
     public void registerCategories(IRecipeCategoryRegistration registration) {
-        registration.addRecipeCategories(
-                new ElectricCrusherRecipeCategory(registration.getJeiHelpers().getGuiHelper())
-        );
+        registration.addRecipeCategories(new ElectricCrusherRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
     }
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
         RecipeManager recipeManager = Minecraft.getInstance().level.getRecipeManager();
 
-        List<ElectricCrusherRecipe> electricCrusherRecipes = recipeManager.getAllRecipesFor(ModRecipes.ELECTRIC_CRUSHER_TYPE.get()).stream().map(RecipeHolder::value).toList();
+        List<ElectricCrusherRecipe> electricCrusherRecipes = recipeManager.getAllRecipesFor(ModRecipes.ELECTRIC_CRUSHER_TYPE.get())
+                .stream()
+                .map(RecipeHolder::value)
+                .toList();
 
         registration.addRecipes(ElectricCrusherRecipeCategory.ELECTRIC_CRUSHER_RECIPE_RECIPE_TYPE, electricCrusherRecipes);
     }
--- src/main/java/com/teamofpowersim/powersim/justenoughitems/categories/ElectricCrusherRecipeCategory.java
+++ src/main/java/com/teamofpowersim/powersim/justenoughitems/categories/ElectricCrusherRecipeCategory.java
@@ -1,5 +1,6 @@
 package com.teamofpowersim.powersim.justenoughitems.categories;
 
+import com.mojang.logging.LogUtils;
 import com.teamofpowersim.powersim.PowerSim;
 import com.teamofpowersim.powersim.block.ModBlocks;
 import com.teamofpowersim.powersim.recipe.crusher.ElectricCrusherRecipe;
@@ -8,19 +9,21 @@ import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
-import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
 
 public class ElectricCrusherRecipeCategory implements IRecipeCategory<ElectricCrusherRecipe> {
     public static final ResourceLocation UID = ResourceLocation.fromNamespaceAndPath(PowerSim.MODID, "crushing");
     public static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(PowerSim.MODID, "textures/gui/crusher/electric_crusher_gui.png");
 
     public static final RecipeType<ElectricCrusherRecipe> ELECTRIC_CRUSHER_RECIPE_RECIPE_TYPE = new RecipeType<>(UID, ElectricCrusherRecipe.class);
+
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     private final IDrawable background;
     private final IDrawable icon;
@@ -46,8 +49,13 @@ public class ElectricCrusherRecipeCategory implements IRecipeCategory<ElectricCr
     }
 
     @Override
+    public IDrawable getBackground() {
+        return background;
+    }
+
+    @Override
     public void setRecipe(IRecipeLayoutBuilder builder, ElectricCrusherRecipe recipe, IFocusGroup focuses) {
-        builder.addSlot(RecipeIngredientRole.INPUT, 54, 34).addIngredients(recipe.getIngredients().getFirst());
-        builder.addSlot(RecipeIngredientRole.OUTPUT, 104, 34).addItemStack(recipe.getResultItem(null));
+        builder.addInputSlot(54, 34).addIngredients(recipe.input());
+        builder.addOutputSlot(104, 34).addItemStack(recipe.output());
     }
 }
